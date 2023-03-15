3f60e1e39a595242de6f191deeec46c32e803812
Kevin Kim <kevindkim723@gmail.com>
2023-03-15 14:06:37 -0700

    csan includes the 2 input variant of carry-save adder

--- src/ieu/bmu/csan.sv
+++ src/ieu/bmu/csan.sv
@@ -2,3 +2,7 @@ module csan(input logic a0, input logic a1, input logic a2, output logic c, outp
     assign c = (a0&a1) | (a0&a2) | (a1&a2);
     assign s = a0 ^ a1 ^ a2;
 endmodule
+module csan2(input logic a0, input logic a1, output logic c, output logic s);
+    assign c = (a0&a1);
+    assign s = a0 ^ a1;
+endmodule
