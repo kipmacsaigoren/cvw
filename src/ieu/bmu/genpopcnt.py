5b1934c984b0798280932952b04fcd5c05be7543
Kevin Kim <kevindkim723@gmail.com>
2023-03-14 18:38:14 -0700

    progress on python script

--- src/ieu/bmu/genpopcnt.py
+++ src/ieu/bmu/genpopcnt.py
@@ -20,6 +20,12 @@ class Weight:
 
 import math
 
+def csa(width, i1,j1,k1,i2,j2,k2,carryidx, sumidx,i3="X",j3="X",k3="X"):
+    if (width == 2):
+        return f"csa csa{i1}_{j1}_{k1}_{i2}_{j2}_{k2}_{i3}_{j3}_{k3}(w{i1}_{j1}_{k1}, w{i2}_{j2}_{k2}, 0, w{i_1+1}_{0}_{carryidx}, w{i_1}_{j_1+1}_{sumidx});\n"
+    elif (width == 3):
+        return f"csa csa{i1}_{j1}_{k1}_{i2}_{j2}_{k2}_{i3}_{j3}_{k3}(w{i1}_{j1}_{k1}, w{i2}_{j2}_{k2}, w{i3}_{j3}_{k3}, w{i_1+1}_{0}_{carryidx}, w{i_1}_{j_1+1}_{sumidx});\n"
+
 def generate(XLEN):
     lines = ""
     lines += f"""
@@ -35,14 +41,73 @@ def generate(XLEN):
     carry_queue = []
 
 
-
     lines += "assign {"
     for i in reversed(range(XLEN)):
         lines += f"w{0}_{0}_{i}, "
-        pairs[0][0][i] = True
+        w = Weight(0,0,i)
+        sum_queue.append(w)
     
     # strip trailing comma and space
     lines = lines[0:-2]
+
+    while (len(sum_queue) > 0 or len(carry_queue) > 0):
+        carryidx=0
+        while (len(sum_queue) > 0):
+            if (len(sum_queue) == 1):
+                w1 = sum_queue[0]
+                sum_queue=sum_queue[1:]
+
+                # extract indices
+                i1 = w1.i
+                j1 = w1.j
+                k1 = w1.k
+
+                lines += f"assign s{i1} = w{i1}_{j1}_{k1};"
+                
+            elif (len(sum_queue) == 2):
+                # dequeue two weights
+                w1 = sum_queue[0]
+                w2 = sum_queue[1]
+                sum_queue = sum_queue[2:]
+                
+                # extract indices
+                i1 = w1.i
+                j1 = w1.j
+                k1 = w1.k
+
+                i2 = w2.i
+                j2 = w2.j
+                k2 = w2.k
+
+                l = csa(2, i1,j1,k1,i2,j2,k2, carryidx,k, i_1+XLEN)
+                lines += l
+                
+            else:
+                # dequeue three weights
+                w1 = sum_queue[0]
+                w2 = sum_queue[1]
+                w3 = sum_queue[2]
+                sum_queue = sum_queue[3:]
+                
+                # extract indices
+                i1 = w1.i
+                j1 = w1.j
+                k1 = w1.k
+
+                i2 = w2.i
+                j2 = w2.j
+                k2 = w2.k
+
+                i3 = w3.i
+                j3 = w3.j
+                k3 = w3.k
+
+                l = csa(3, i1,j1,k1,i2,j2,k2, carryidx,k, i_1+XLEN,i3,j3,k3)
+                lines += l
+                
+
+
+
     
     index = 0
     
