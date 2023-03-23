a2a78602623306bf1e0df66f7747a1f9d5dd8328
Kevin Kim <kevindkim723@gmail.com>
2023-03-23 07:14:51 -0700

    csa optimized

--- src/ieu/bmu/csan.sv
+++ src/ieu/bmu/csan.sv
@@ -1,5 +1,6 @@
 module csan(input logic a0, input logic a1, input logic a2, output logic c, output logic s);
-    assign c = (a0&a1) | (a0&a2) | (a1&a2);
+    assign c = a0&(a1 | a2) + a2&(a1 | a0);
+    //assign c = (a0 & a1) | (a1&a2) | (a2&a0);
     assign s = a0 ^ a1 ^ a2;
 endmodule
 module csan2(input logic a0, input logic a1, output logic c, output logic s);
