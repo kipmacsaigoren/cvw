efc18b0502333d00c9fd430621d727bb01dd5cdc
Kevin Kim <kevindkim723@gmail.com>
2023-04-30 01:56:11 -0700

    help

--- src/ieu/alu.sv
+++ src/ieu/alu.sv
@@ -61,7 +61,11 @@ module alu #(parameter WIDTH=`XLEN) (
   
   // Shifts (configurable for rotation)
   shifter sh(.A, .Amt(B[`LOG_XLEN-1:0]), .Right(Funct3[2]), .W64, .SubArith, .Y(Shift), .Rotate(BALUControl[2]));
-
+  logic [WIDTH-1:0]af,bf,afs,bfs;
+  assign af = {A[WIDTH-1] ^ 1'b0, A[WIDTH-2:0]};
+  assign bf = {B[WIDTH-1] ^ 1'b0, B[WIDTH-2:0]};
+  assign afs = {A[WIDTH-1] ^ 1'b1, A[WIDTH-2:0]};
+  assign bfs = {B[WIDTH-1] ^ 1'b1, B[WIDTH-2:0]};
   // Condition code flags are based on subtraction output Sum = A-B.
   // Overflow occurs when the numbers being subtracted have the opposite sign 
   // and the result has the opposite sign of A.
@@ -69,8 +73,12 @@ module alu #(parameter WIDTH=`XLEN) (
   assign Neg  = Sum1[WIDTH-1];
   assign Asign = A[WIDTH-1];
   assign Bsign = B[WIDTH-1];
-  assign LT = Asign & ~Bsign | Asign & Neg | ~Bsign & Neg; 
-  assign LTU = ~Carry1;
+
+  //assign LT = Asign & ~Bsign | Asign & Neg | ~Bsign & Neg; 
+  //assign LTU = ~Carry1;
+  
+  assign LT = (afs<bfs);
+  assign LTU = (af<bf);
  
   // Select appropriate ALU Result
   always_comb begin
--- synthDC/bmuscript2.sh
+++ synthDC/bmuscript2.sh
@@ -1,6 +1,209 @@
-make synth DESIGN=wallypipelinedcore TECH=tsmc28psyn FREQ=10000 CONFIG=rv32i
-make synth DESIGN=wallypipelinedcore TECH=tsmc28psyn FREQ=10000 CONFIG=rv64i
+#make synth DESIGN=wallypipelinedcore TECH=tsmc28psyn FREQ=10000 CONFIG=rv32i
+#make synth DESIGN=wallypipelinedcore TECH=tsmc28psyn FREQ=10000 CONFIG=rv64i
 #make synth DESIGN=alu TECH=tsmc28psyn FREQ=10000 CONFIG=rv32i MAXOPT=1
 #make synth DESIGN=alu TECH=tsmc28psyn FREQ=10000 CONFIG=rv64i MAXOPT=1
 #make synth DESIGN=alu TECH=tsmc28psyn FREQ=100 CONFIG=rv32i MAXOPT=1
 #make synth DESIGN=alu TECH=tsmc28psyn FREQ=100 CONFIG=rv64i MAXOPT=1
+rv64gc=$WALLY/config/rv64gc/wally-config.vh
+rv64i=$WALLY/config/rv64i/wally-config.vh
+rv32gc=$WALLY/config/rv32gc/wally-config.vh
+rv32i=$WALLY/config/rv32i/wally-config.vh
+FREQ=10000
+
+
+
+
+#alu32 none
+sed -i '149s/.*/`define ZBA_SUPPORTED 0/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 0/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 0/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 0/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+
+#alu32 zba
+sed -i '149s/.*/`define ZBA_SUPPORTED 1/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 0/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 0/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 0/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+#alu32 zbb
+sed -i '149s/.*/`define ZBA_SUPPORTED 0/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 1/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 0/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 0/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+#alu32 zbc
+sed -i '149s/.*/`define ZBA_SUPPORTED 0/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 0/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 1/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 0/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+#alu32 zbs
+sed -i '149s/.*/`define ZBA_SUPPORTED 0/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 0/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 0/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 1/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+#alu32 zb*
+sed -i '149s/.*/`define ZBA_SUPPORTED 1/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 1/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 1/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 1/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+#alu64 none
+sed -i '151s/.*/`define ZBA_SUPPORTED 0/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 0/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 0/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 0/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+#alu64 zba
+sed -i '151s/.*/`define ZBA_SUPPORTED 1/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 0/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 0/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 0/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+#alu64 zbb
+sed -i '151s/.*/`define ZBA_SUPPORTED 0/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 1/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 0/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 0/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+#alu64 zbc
+sed -i '151s/.*/`define ZBA_SUPPORTED 0/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 0/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 1/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 0/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+#alu64 zbs
+sed -i '151s/.*/`define ZBA_SUPPORTED 0/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 0/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 0/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 1/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ MAXOPT=1
+
+sleep 1m
+#alu64 zb*
+sed -i '151s/.*/`define ZBA_SUPPORTED 1/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 1/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 1/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 1/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ MAXOPT=1
+
+######
+FREQ=10001
+
+#alu32 none
+sed -i '149s/.*/`define ZBA_SUPPORTED 0/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 0/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 0/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 0/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ 
+
+sleep 1m
+
+#alu32 zba
+sed -i '149s/.*/`define ZBA_SUPPORTED 1/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 0/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 0/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 0/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ 
+
+sleep 1m
+#alu32 zbb
+sed -i '149s/.*/`define ZBA_SUPPORTED 0/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 1/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 0/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 0/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ 
+
+sleep 1m
+#alu32 zbc
+sed -i '149s/.*/`define ZBA_SUPPORTED 0/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 0/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 1/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 0/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ 
+
+sleep 1m
+#alu32 zbs
+sed -i '149s/.*/`define ZBA_SUPPORTED 0/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 0/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 0/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 1/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ 
+
+sleep 1m
+#alu32 zb*
+sed -i '149s/.*/`define ZBA_SUPPORTED 1/' $rv32i
+sed -i '150s/.*/`define ZBB_SUPPORTED 1/' $rv32i
+sed -i '151s/.*/`define ZBC_SUPPORTED 1/' $rv32i
+sed -i '152s/.*/`define ZBS_SUPPORTED 1/' $rv32i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv32i FREQ=$FREQ 
+
+sleep 1m
+#alu64 none
+sed -i '151s/.*/`define ZBA_SUPPORTED 0/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 0/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 0/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 0/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ 
+
+sleep 1m
+#alu64 zba
+sed -i '151s/.*/`define ZBA_SUPPORTED 1/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 0/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 0/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 0/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ 
+
+sleep 1m
+#alu64 zbb
+sed -i '151s/.*/`define ZBA_SUPPORTED 0/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 1/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 0/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 0/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ 
+
+sleep 1m
+#alu64 zbc
+sed -i '151s/.*/`define ZBA_SUPPORTED 0/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 0/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 1/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 0/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ 
+
+sleep 1m
+#alu64 zbs
+sed -i '151s/.*/`define ZBA_SUPPORTED 0/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 0/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 0/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 1/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ 
+
+sleep 1m
+#alu64 zb*
+sed -i '151s/.*/`define ZBA_SUPPORTED 1/' $rv64i
+sed -i '152s/.*/`define ZBB_SUPPORTED 1/' $rv64i
+sed -i '153s/.*/`define ZBC_SUPPORTED 1/' $rv64i
+sed -i '154s/.*/`define ZBS_SUPPORTED 1/' $rv64i
+make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ 
