2073d8cf44b7d9541abb3ea1481731c0017cd684
Kevin Kim <kevindkim723@gmail.com>
2023-04-30 13:45:37 -0700

    updated master alu script

--- synthDC/masteraluscript.sh
+++ synthDC/masteraluscript.sh
@@ -4,7 +4,7 @@ rv64gc=$WALLY/config/rv64gc/wally-config.vh
 rv64i=$WALLY/config/rv64i/wally-config.vh
 rv32gc=$WALLY/config/rv32gc/wally-config.vh
 rv32i=$WALLY/config/rv32i/wally-config.vh
-FREQ=10000
+FREQ=10100
 
 #alu32 none
 sed -i '149s/.*/`define ZBA_SUPPORTED 0/' $rv32i
@@ -278,6 +278,7 @@ sed -i '153s/.*/`define ZBC_SUPPORTED 1/' $rv64i
 sed -i '154s/.*/`define ZBS_SUPPORTED 1/' $rv64i
 make synth DESIGN=alu TECH=tsmc28psyn CONFIG=rv64i FREQ=$FREQ MAXOPT=1
 
+((FREQ++))
 #alu32 none
 sed -i '149s/.*/`define ZBA_SUPPORTED 0/' $rv32i
 sed -i '150s/.*/`define ZBB_SUPPORTED 0/' $rv32i
